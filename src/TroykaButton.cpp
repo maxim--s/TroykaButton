e5a0fa775aeac7b3a25eca97479a2df06c6b78e0
Maxim S. Shatskih <maxim@storagecraft.com>
2020-08-20 18:09:52 +0300

    Code complete, self-review done, quick tested

--- src/TroykaButton.cpp
+++ src/TroykaButton.cpp
@@ -1,75 +1,177 @@
 /****************************************************************************/
 //  Function:       Cpp file for TroykaButton
-//  Arduino IDE:    Arduino-1.8.5
-//  Author:         Igor Dementiev
-//  Date:           Sep 09,2018
-//  Version:        v1.0
-//  by www.amperka.ru
+//  Arduino IDE:    Arduino-1.8.13
+//  Author:         Igor Dementiev and Maxim Shatskih
+//  Date:           Aug 14,2020
+//  Version:        v2.0
+//  by www.amperka.ru and contributors
 /****************************************************************************/
 
 #include "TroykaButton.h"
 
-TroykaButton::TroykaButton(uint8_t pin, uint32_t timeHold, bool pullUP) {
-    _pin = pin;
-    _timeHold = timeHold;
-    _pullUP = pullUP;
+// Тела приватных инлайнов
+
+// != для типа bool есть xor
+bool TroykaButton::_readPinState() const { return (digitalRead(_pin) == HIGH) != _pullUP; }
+
+void TroykaButton::_updatePinState(bool newPinState) {
+  _pinState = newPinState;
+  _tmPinState = millis();
 }
 
-// инициализация кнопки
-void TroykaButton::begin() {
-    pinMode(_pin, INPUT);
-    _msButtonState = 0;
-    _buttonStateOld = ON_RELEASE;
-	_isClick = false;
+void TroykaButton::_updateState(_State newState) {
+  _prevState = _state;
+  _state = newState;
+  _isStateDirty = true;
+  _tmState = millis();
 }
 
-// считывание состояние кнопки
+// API
+
+// pin - номер пина
+// timeHold - время (в ms) непрерывного удержания кнопки,
+//  после которого нажатие понимается как долгое удержание
+// pullUP - должен быть true, если пин оттянут резистором к питанию,
+//  и false - если к земле
+TroykaButton::TroykaButton(uint8_t pin, uint32_t timeHold, bool pullUP) :
+    _pin(pin),
+    _timeHold(timeHold),
+    _pullUP(pullUP) {
+  _resetObject();
+}
+
+// считывание данных с кнопки
+// обновляет внутреннее состояние объекта на основании состояния пина
+// для вызова в loop() каждый раз
+// метод неблокирующий (внутри нет delay() и аналогов)
 void TroykaButton::read() {
-    _stateButton = false;
-    _isClick = false;
-    bool buttonStateNow = !digitalRead(_pin);
-    if (!_pullUP) {
-        buttonStateNow = !buttonStateNow;
+  // для начала всегда отработаем длинное удержание,
+  //  даже если активно подавление дребезга
+  //  (для этого не нужно щупать пин)
+  // если после подавления дребезга кнопка окажется отпущенной -
+  //  состояние обновится еще раз ниже
+  if (_state == stPressed && millis() - _tmState >= _timeHold)
+    // после этого _state более не stPressed, второй раз код не выполнится
+    _updateState(stLongHold);
+  // щупаем пин
+  bool currentPinState = _readPinState();
+  if (currentPinState != _pinState) {
+    // пин изменился
+    // (ре)стартуем таймаут подавления дребезга
+    //  с новым (возможно, шумовым и не окончательным) состоянием пина
+    _isDebounceTimeoutActive = true;
+    _updatePinState(currentPinState);
+  } else {
+    // пин не изменился
+    if (_isDebounceTimeoutActive && millis() - _tmPinState >= DEBOUNCE_TIME) {
+      // подавление дребезга активно, а пин не менялся в течение всего таймаута
+      //  (если бы он менялся, код выше рестартнул бы таймаут)
+      // _pinState теперь суть окончательное состояние пина
+      // подавление дребезга завершилось
+      _isDebounceTimeoutActive = false;
+      // делаем реальную работу
+      _State newState = _pinState ? stPressed : stReleased;
+      // switch() ниже нужен для следующего сценария:
+      //  - если контакты удерживаемой кнопки кратковременно разомкнулись
+      //      (из-за низкого качества кнопки, окисления и др)
+      //  - в этом случае дважды стартанет подавление дребезга,
+      //      и, после прекращения ложного размыкания
+      //          (и истечения DEBOUNCE_TIME после того)
+      //      мы попадем сюда с newState == stPressed и _state == stPressed или stLongHold
+      //  - если убрать switch(),
+      //      то в вышеуказанном сценарии _updateState() взведет _isStateDirty,
+      //      а следом justPressed() представит ложное размыкание как второе нажатие
+      //          (без промежуточного отпускания)
+      //          БАГ
+      //  - кроме того, _updateState() рестартнет _tmState,
+      //      и ложное размыкание рестартнет таймаут timeHold
+      //          ВТОРОЙ БАГ
+      //  - примерно то же самое произойдет и в случае (хотя и малореальном),
+      //      если отпущенная кнопка кратковременно замкнулась
+      //      (из-за краткого КЗ в проводах на макетке и др)
+      //  - а потому - switch(), избавляющий от _updateState()
+      //      (взвода _isStateDirty и рестарта _tmState)
+      //      в этих сценариях
+      //  - если вкратце: switch() отрабатывает сценарий,
+      //     когда пин подребезжал-подребезжал, и вернулся в старое состояние
+      switch (_state) {
+      case stReleased:
+        if (newState == stReleased)
+          // больше нечего делать
+          return;
+        break;
+      case stPressed:
+      case stLongHold:
+        if (newState == stPressed)
+          return;
+        break;
+      }
+      // и вот теперь наконец обновляем "софтовое" состояние кнопки
+      _updateState(newState);
     }
-    if (buttonStateNow && buttonStateNow != _buttonStateWas && millis() - _msButtonState > DEBOUNCE_TIME) {
-        _buttonStateNowLong = false;
-        _msButtonState = millis();
-        _stateButton = ON_PRESS;
-        _buttonStateOld =  _stateButton;
-    }
-    if (!buttonStateNow && buttonStateNow != _buttonStateWas && millis() - _msButtonState > DEBOUNCE_TIME) {
-        _msButtonState = millis();
-        _stateButton = ON_RELEASE;
-    }
-    if (buttonStateNow && !_buttonStateNowLong && millis() - _msButtonState > _timeHold) {
-        _buttonStateNowLong = true;
-        _msButtonState = millis();
-        _stateButton = ON_PRESS_LONG;
-        _buttonStateOld =  _stateButton;
-    }
-    if (_stateButton == ON_RELEASE && _buttonStateOld == ON_PRESS ) {
-		_buttonStateOld = ON_RELEASE;
-        _isClick = true;
-    }
-    _buttonStateWas = buttonStateNow;
-}
-
-// определение клика кнопки
-bool TroykaButton::justPressed() const {
-    return _stateButton == ON_PRESS ? 1 : 0;
-}
-
-// определение отжатие кнопки
-bool TroykaButton::justReleased() const {
-    return _stateButton == ON_RELEASE ? 1 : 0;
-}
-
-// определение зажатие кнопки (по умолчанию 2 секунды)
-bool TroykaButton::isHold() const {
-    return _stateButton == ON_PRESS_LONG ? 1 : 0;
+    //else
+    //  - read() ничего не делает вне таймаута подавления дребезга (или пока он не истек),
+    //     если пин не менялся
+  }
 }
 
 // определение короткого клика, если сработал метод isHold() клик не сработает.
-bool TroykaButton::isClick() const {
-    return _isClick;
+// то же, что и justReleased() (именно так! для совместимости со старым кодом)
+//  с той лишь разницей, что justReleased() вернет true
+//  и в случае долгого (timeHold) удержания кнопки,
+//  а этот метод в таком случае вернет false
+// isClick() не до конца совместим с justReleased(),
+//   а именно, если isClick() уже вернул true, то justReleased() вернет false
+//   а если justReleased() уже вернул true, то isClick() вернет false
+// возможно совместное использование isClick() и justReleased() в виде:
+//   - первым в loop() должен зваться isClick()
+//   - justReleased() должен зваться, только если isClick() вернул false
+//   - тогда, если justReleased() вернет true,
+//       это будет означать "отпускание после долгого удержания"
+//   - результат isClick() при этом можно проигнорировать
+bool TroykaButton::isClick() {
+  if (_state == stReleased && _isStateDirty && _prevState == stPressed) {
+    // мы прочитали вновь установленное состояние
+    _isStateDirty = false;
+    return true;
+  }
+  return false;
+}
+
+// внутренности
+
+// внутренний движок под конструктором и reinit()
+// инициализирует все поля состояния
+void TroykaButton::_resetObject() {
+  // при инициализации считаем, что пин не взведен
+  // если же он на самом деле взведен - то первый же read() в loop() или в reinit()
+  //  начнет таймаут подавления дребезга,
+  //  который в итоге (если кнопка действительно нажата) приведет к тому же,
+  //  что и нажатие кнопки сразу по возврату из конструктора или reinit()
+  _pinState = false;
+  // поскольку мы устанавливаем _isDebounceTimeoutActive в false,
+  //  по факту безразлично, как инициализировать это поле
+  _tmPinState = 0;
+  // таймаут активируется первый раз при первом же чтении взведенного пина в read()
+  _isDebounceTimeoutActive = false;
+  // при инициализации считаем, что кнопка не нажата
+  // далее см. комментарий выше к _pinState
+  _state = stReleased;
+  // поскольку мы устанавливаем _isStateDirty в false,
+  //  по факту безразлично, как инициализировать это поле, нужное только для isClick()
+  _prevState = stReleased;
+  _isStateDirty = false;
+  // поскольку мы устанавливаем _state в stReleased,
+  //  по факту безразлично, как инициализировать это поле
+  _tmState = 0;
+}
+
+// единый, для экономии места под код, внутренний движок под всеми justXxx()
+bool TroykaButton::_justXxxInner(_State stateToTest) {
+  if (_state == stateToTest && _isStateDirty) {
+    // мы прочитали вновь установленное состояние
+    _isStateDirty = false;
+    return true;
+  }
+  return false;
 }
--- src/TroykaButton.h
+++ src/TroykaButton.h
@@ -1,10 +1,10 @@
 /****************************************************************************/
 //  Function:       Header file for TroykaButton
-//  Arduino IDE:    Arduino-1.8.5
-//  Author:         Igor Dementiev
-//  Date:           Sep 09,2018
-//  Version:        v1.0
-//  by www.amperka.ru
+//  Arduino IDE:    Arduino-1.8.13
+//  Author:         Igor Dementiev and Maxim Shatskih
+//  Date:           Aug 14,2020
+//  Version:        v2.0
+//  by www.amperka.ru and contributors
 /****************************************************************************/
 
 #ifndef _TROYKA_BUTTON_H_
@@ -16,46 +16,172 @@
 #include <WProgram.h>
 #endif
 
+//
+//  ВНИМАНИЕ:
+//
+// - если кнопка была аппаратно нажата в момент выполнения конструктора или reinit(),
+//     например, если Arduino включили с нажатой кнопкой,
+//    то произойдет то же самое, как если бы кнопка была отпущена,
+//    а нажали ее сразу после возврата из конструктора или reinit()
+// - эта логика необходима для реализации подавления дребезга,
+//    с учетом того, что в конструкторе, как правило, невозможно прочесть состояние пина
+//
+
+//
+//              Логика подавления дребезга
+//
+// - ожидается, пока электрическое состояние пина не менялось в течение DEBOUNCE_TIME
+// - после этого состояние пина считается окончательным
+//
+
+// время подавления дребезга в ms
 #define DEBOUNCE_TIME   50
-#define ON_PRESS        1
-#define ON_PRESS_LONG   2
-#define ON_RELEASE      3
 
 class TroykaButton
 {
-public:
-    TroykaButton(uint8_t pin, uint32_t timeHold = 2000, bool pullUP = true);
-    // инициализация кнопки
-    void begin();
-    // считывание данных с кнопки
-    void read();
-    // определение клика кнопки
-    bool justPressed() const;
-    // определение отжатие кнопки
-    bool justReleased() const;
-    // определение зажатие кнопки (по умолчанию 2 секунды)
-    bool isHold() const;
-    // определение короткого клика, если сработал метод isHold() клик не сработает.
-    bool isClick() const;
-private:
-    // номера пина
-    uint8_t _pin;
-    // время длительного зажатия кнопки
-    uint32_t _timeHold;
-    // выбор подтяжки
-    bool _pullUP;
-    // короткое нажатие кнопки (клик)
-    bool _isClick;
-    // состояние кнопки
-    uint8_t _stateButton;
-    // предыдущие состояние кнопки
-    uint8_t _buttonStateOld;
-    // ранее состояние кнопки
-    bool _buttonStateWas;
-    // длинное нажатие
-    bool _buttonStateNowLong;
-    // время нажатия кнопки
-    uint32_t _msButtonState;
+public: // API
+  // pin - номер пина
+  // timeHold - время (в ms) непрерывного удержания кнопки,
+  //  после которого нажатие понимается как долгое удержание
+  // pullUP - должен быть true, если пин оттянут резистором к питанию,
+  //  и false - если к земле
+  TroykaButton(uint8_t pin, uint32_t timeHold = 2000, bool pullUP = true);
+  // полный сброс объекта, после которого он становится таким же,
+  //  как сразу после конструктора + begin() + единственного read()
+  // повторно звать begin() - не требуется
+  // новый таймаут от setTimeHold() НЕ сбрасывается к старому
+  // может быть полезен в случае, если на кнопку временно повесили прерывание,
+  //  а затем вернулись к использованию этого класса
+  inline void reinit() __attribute__((always_inline));
+  // изменяет заданное в конструкторе время (в ms) непрерывного удержания кнопки,
+  //  после которого нажатие понимается как долгое удержание
+  // может быть полезен в случае, если используется логика нажатий на кнопку
+  //  азбукой Морзе (или как-то вроде), как в некоторых брелках автосигнализаций
+  inline void setTimeHold(uint32_t newTimeHold) __attribute__((always_inline));
+  // инициализация кнопки (для вызова в setup())
+  inline void begin() __attribute__((always_inline));
+  // считывание данных с кнопки
+  // обновляет внутреннее состояние объекта на основании состояния пина
+  // для вызова в loop() каждый раз
+  // метод неблокирующий (внутри нет delay() и аналогов)
+  void read();
+  // определение клика кнопки
+  // возвращает true тогда и только тогда, когда:
+  //  1) ранее отпущенная кнопка была нажата
+  //  И
+  //  2) только при первом вызове после того, как выполнилось условие 1)
+  // возвращает false во всех остальных случаях
+  // можно сказать, что этот метод работает "по фронту"
+  // метод неблокирующий (внутри нет delay() и аналогов)
+  inline bool justPressed() __attribute__((always_inline));
+  // определение отжатия кнопки
+  // то же, что и justPressed(), но условие 1) выглядит как:
+  //  "ранее нажатая кнопка была отпущена"
+  inline bool justReleased() __attribute__((always_inline));
+  // определение удержания кнопки
+  // то же, что и justPressed(), но условие 1) выглядит как:
+  //  "нажатая кнопка удерживалась нажатой в течение >= timeHold миллисекунд"
+  inline bool justHeld() __attribute__((always_inline));
+  // true, если кнопка нажата "здесь и сейчас", иначе false
+  // метод неблокирующий (внутри нет delay() и аналогов)
+  inline bool isPressed() const __attribute__((always_inline));
+  // true, если кнопка отпущена "здесь и сейчас", иначе false
+  // метод неблокирующий (внутри нет delay() и аналогов)
+  inline bool isReleased() const __attribute__((always_inline));
+  // true, если кнопка нажата "здесь и сейчас",
+  //  и удерживалась нажатой в течение >= timeHold миллисекунд, иначе false
+  // метод неблокирующий (внутри нет delay() и аналогов)
+  inline bool isHold() const __attribute__((always_inline));
+  // определение короткого клика, если сработал метод isHold() клик не сработает.
+  // то же, что и justReleased() (именно так! для совместимости со старым кодом)
+  //  с той лишь разницей, что justReleased() вернет true
+  //  и в случае долгого (timeHold) удержания кнопки,
+  //  а этот метод в таком случае вернет false
+  // isClick() не до конца совместим с justReleased(),
+  //   а именно, если isClick() уже вернул true, то justReleased() вернет false
+  //   а если justReleased() уже вернул true, то isClick() вернет false
+  // возможно совместное использование isClick() и justReleased() в виде:
+  //   - первым в loop() должен зваться isClick()
+  //   - justReleased() должен зваться, только если isClick() вернул false
+  //   - тогда, если justReleased() вернет true,
+  //       это будет означать "отпускание после долгого удержания"
+  //   - результат isClick() при этом можно проигнорировать
+  bool isClick();
+private: // данные инициализации
+  // номер пина
+  const uint8_t _pin;
+  // время длительного зажатия кнопки
+  uint32_t _timeHold;
+  // выбор подтяжки
+  const bool _pullUP;
+private: // данные состояния
+  enum _State {
+    stReleased,
+    stPressed,
+    stLongHold, // во всех современных компиляторах можно ставить здесь запятую
+  };
+  // запомненное электрическое состояние пина (с поправкой на _pullUP)
+  bool _pinState;
+  // millis(), когла было прочитано и сохранено _pinState
+  unsigned long _tmPinState;
+  // true, если течет временной интервал подавления дребезга, иначе false
+  bool _isDebounceTimeoutActive;
+  // текущее "софтовое" состояние кнопки
+  _State _state;
+  // предыдущее состояние кнопки (используется только в isClick())
+  _State _prevState;
+  // true, если _state изменилось, и после этого еще не был вызван
+  //  один из justXxx() методов или isClick()
+  bool _isStateDirty;
+  // millis() последнего обновления _state
+  // нужно для таймаута удержания, мы не можем использовать _tmPinState
+  //  для него, ибо тогда ложное кратковременное дребезжащее размыкание нажатой кнопки
+  //  сбросит логику timeHold
+  unsigned long _tmState;
+private: // внутренности
+  // внутренний движок под конструктором и reinit()
+  // инициализирует все поля состояния
+  void _resetObject();
+  // единый, для экономии места под код, внутренний движок под всеми justXxx()
+  bool _justXxxInner(_State stateToTest);
+  // читает электрическое состояние пина (с поправкой на _pullUP)
+  inline bool _readPinState() const __attribute__((always_inline));
+  // запоминает состояние пина правильным образом
+  inline void _updatePinState(bool newPinState) __attribute__((always_inline));
+  // oбновляет _state правильным образом
+  inline void _updateState(_State newState) __attribute__((always_inline));
 };
 
-#endif
+// Тела публичных инлайнов
+
+// они настолько крошечны
+//  (а приватные из них редко зовутся по количеству мест в коде),
+//  что есть реальные основания полагать,
+//  что они меньше прологов-эпилогов функций
+//   (а некоторые ненамного длиннее опкода call)
+// потому, для экономии места под код - инлайны
+// естественно, производительность тоже повышается
+
+void TroykaButton::reinit() {
+  _resetObject();
+  begin(); // мало ли что успели понаделать с этим пином
+  read(); // для ре-инициализации подавления дребезга
+}
+
+void TroykaButton::setTimeHold(uint32_t newTimeHold) { _timeHold = newTimeHold; }
+
+void TroykaButton::begin() { pinMode(_pin, INPUT); }
+
+bool TroykaButton::justPressed() { return _justXxxInner(stPressed); }
+
+bool TroykaButton::justReleased() { return _justXxxInner(stReleased); }
+
+bool TroykaButton::justHeld() { return _justXxxInner(stLongHold); }
+
+bool TroykaButton::isPressed() const { return _state == stPressed || _state == stLongHold; }
+
+bool TroykaButton::isReleased() const { return _state == stReleased; }
+
+bool TroykaButton::isHold() const { return _state == stLongHold; }
+
+#endif // _TROYKA_BUTTON_H_
